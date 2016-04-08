a7edac57ba7076c13e876c8b65e98ff01d9a8699
Rene Zarwel <darenegade@gmx.de>
2016-04-08 16:03:43 +0200

    Erosion und Delatation - läuft noch nicht

--- CGBVRahmenprogramm/A1_Bildverarbeitung/FragmentShaderDilatation.glsl
+++ CGBVRahmenprogramm/A1_Bildverarbeitung/FragmentShaderDilatation.glsl
@@ -18,19 +18,33 @@ out vec4 fragColor;					// Ausgabewert mit 4 Komponenten zwischen 0.0 und 1.0
 uniform sampler2DRect textureMap;		// Sampler für die Texture Map
 uniform vec4 param1;				// param1.x +=F5, -=F6, param1.y +=F7, -=F8, param1.z +=F9, -=F10, param1.w +=F11, -=F12
 
-uniform vec2 offsets[9] = vec2[](	vec2(-1,  1),
-									vec2(-1,  0),
-									vec2(-1, -1),
-									vec2( 0,  1),
-									vec2( 0,  0),
-									vec2( 0, -1),
-									vec2( 1,  1),
-									vec2( 1,  0),
-									vec2( 1, -1)	);
+uniform vec2 resolution;
 
 void main()
 {
+	int dist = (int) param1.x;
+	vec2 coords;
+	
+	vec4 MaxValue = vec4( 0.0, 0.0, 0.0, 0.0 );
 
-    fragColor =	texture(textureMap, texCoords);
+	for( int x = 0 - dist; x <= dist; x++ ){
+		for( int y = 0 - dist; y <= dist; y++ ){
+
+			coords = texCoords + vec2(x,y);
+
+			if(coords.x < 0)
+				coords.x = 0;
+			if(coords.y < 0)
+				coords.y = 0;
+			if(coords.x > resolution.x)
+				coords.x = resolution.x;
+			if(coords.y > resolution.y)
+				coords.y = resolution.y;
+
+			MaxValue = max( texture( textureMap, coords), MinValue );
+
+		}
+	}
+	fragColor = MaxValue;
 
 }
